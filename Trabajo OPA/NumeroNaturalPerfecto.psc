6c07ac31cd408977fd8d00e4a1df0b3861f7d615
Pecheche <53908266+Pecheche@users.noreply.github.com>
2019-09-20 09:10:49 -0500

    Prueba OPA

---
+++ Trabajo OPA/NumeroNaturalPerfecto.psc
@@ -0,0 +1,38 @@
+Algoritmo sin_titulo
+	Definir suma, n, c, i como entero
+	suma=0
+	n=0
+	c=1
+	i=0
+	Mientras i<3 Hacer
+		Escribir "Ingrese el número"
+		leer n
+		Si n<2^58 y n>0 Entonces
+			Mientras c<n Hacer
+				Si n%c==0 Entonces
+					Escribir sin saltar (ConvertirATexto(c)+",")
+					suma = suma +c
+				Fin Si
+				c = c +1
+			Fin Mientras
+			i=4
+			
+		SiNo
+			i=i+1
+			Escribir "El número invalido"
+		Fin Si
+	Fin Mientras
+	Si i == 1000000000 Entonces
+		Escribir "Error"
+	SiNo
+		Si suma==n Entonces
+			Escribir "El número ",n " es Perfecto"
+		SiNo
+			Si suma<n  Entonces
+				Escribir "El número ",n " es Deficiente"
+			SiNo
+				Escribir "El número ",n " es Abundante"
+			Fin Si
+		Fin Si
+	Fin Si
+FinAlgoritmo
---
+++ Trabajo OPA/Prueba_OPA/Prueba_OPA/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_OPA
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            int c;
+            int i;
+            int n;
+            int suma;
+            suma = 0;
+            n = 0;
+            c = 1;
+            i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Ingrese el número a verificar");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n < Math.Pow(2, 58) && n > 0)
+                {
+                    while (c < n)
+                    {
+                        if (n % c == 0)
+                        {
+                            Console.Write((Convert.ToString(c) + ","));
+                            suma = suma + c;
+                        }
+                        c = c + 1;
+                    }
+                    i = 4;
+                }
+                else
+                {
+                    i = i + 1;
+                    Console.WriteLine("Número invalido");
+                }
+            }
+            if (i == 1000000000)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                if (suma == n)
+                {
+                    Console.WriteLine("El número " + n + " Perfecto");
+                }
+                else
+                {
+                    if (suma < n)
+                    {
+                        Console.WriteLine("El número " + n + " Deficiente");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número " + n + " Abundante");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
